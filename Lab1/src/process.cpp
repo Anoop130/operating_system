a0ab96eba398859a5ee6ade1659a497786afcc4d
Anoop130 <anoopmishra0130@gmail.com>
2025-08-29 00:30:51 -0500

    lab 1 finished

--- Lab1/src/process.cpp
+++ Lab1/src/process.cpp
@@ -25,58 +25,40 @@ namespace cse4733
 
     auto process::execute() -> bool
     {
-        std::optional<int> pid = sysCalls->Fork();
+        std::optional<pid_t> pid = sysCalls->Fork();
 
-        if (pid < 0) {
+        if (!pid.has_value()) {
             std::cerr << "Fork failed" << std::endl;
+            errorMessage = "Failed to create child process.\n";
             std::cout << "Error: " << getErrorMessage() << std::endl;
             return false;
-        } else if (pid == 0) {
+        } else if (pid.value() == 0) {
             executeChildProcess();
             return false;
         } else {
             executeParentProcess(pid.value());
             return true;
         }
-
-        // TODO:
-        // Call the system fork function to create a child process
-        // IF the fork fails, display an error message and return false
-        // IF the fork succeeds, check if this is the child process or the parent process
-        //   IF the child:
-        //     Execute the child process logic
-        //     Return false to indicate that this is the child process
-        //   ELSE the parent:
-        //     Execute the parent process logic
-        //     Return true to indicate that this is the parent process and the child process was created successfully
     }
 
     void process::executeChildProcess()
     {
-        std::cout << "child process ID: " << sysCalls->Getpid() << std::endl;
-        std::cout << "parent process ID: " << sysCalls->Getppid() << std::endl;
+        std::cout << "[CHILD]: child process ID: " << sysCalls->Getpid() << std::endl;
+        std::cout << "[CHILD]: parent process ID: " << sysCalls->Getppid() << std::endl;
 
-        sysCalls->Execvp(executablePath.c_str(), arguments);
-        std::cerr << "Execvp failed" << std::endl;
-        exit(1);
-
-
-        // TODO:
-        //   Print the child process ID
-        //   Print the parent process ID
-        //   Execute the given program with the provided arguments
-        //   Should Execvp return (if it does?) then print an error message and exit
+        if (!sysCalls->Execvp(executablePath.c_str(), arguments)) {
+            // if Execvp returns, it failed
+            std::cerr << "Execvp failed" << std::endl;
+            errorMessage = "Execvp failed\n";
+            exit(1);
+        }
     }
 
     void process::executeParentProcess(pid_t pid)
     {
         this->processId = pid;
-        std::cout << "parent process ID: " << sysCalls->Getpid() << std::endl;
-        std::cout << "child process ID: " << pid << std::endl;
-        // TODO:
-        //   Store the child process ID
-        //   Print the parent process ID
-        //   Print the child process ID
+        std::cout << "[PARENT]: parent process ID: " << sysCalls->Getpid() << std::endl;
+        std::cout << "[PARENT]: child process ID: " << pid << std::endl;
     }
 
     void process::handleChildExitStatus(int status, pid_t pid)
@@ -87,12 +69,6 @@ namespace cse4733
         } else {
             std::cerr << "Child process " << pid << " did not terminate normally" << std::endl;
         }
-        // TODO:
-        // Check if the child process terminated normally
-        // IF the child process terminated normally
-        //    Print a message with the child process ID with the exit status.
-        // ELSE IF the child process did not terminate normally
-        //    Print an error message with the child process ID
     }
 
     auto process::wait() -> std::optional<int>
@@ -102,16 +78,12 @@ namespace cse4733
         if (!result) {
             std::cerr << "Waitpid failed" << std::endl;
             return std::nullopt;
-        } else {
-            handleChildExitStatus(status, this->processId);
-            return WIFEXITED(status);
+        } 
+        handleChildExitStatus(status, this->processId);
+        if (WIFEXITED(status)) {
+            return WEXITSTATUS(status);
         }
-        // Declare a variable to hold the status of the child process
-        // Use the Waitpid system call to wait for the child process to change state (e.g., to terminate)
-        // Check if the Waitpid system call succeeded
-        // If the Waitpid system call failed, print an error message and return std::nullopt
-        // Else if the Waitpid system call succeeded, handle the child's exit status
-        // Return the exit status of the child process
+        
         return std::nullopt;
     }
 
